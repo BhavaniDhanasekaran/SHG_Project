1a4a73df2cf2c6baebcb02f2682a728ac4f788f6
SanthoshN <n.santhoshmca@gmail.com>
2017-04-25 12:26:05 +0000

    Reponsive, logout error page

--- shgapp/views/customError.py
+++ shgapp/views/customError.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render
 import json
 
@@ -5,38 +6,57 @@ import json
 
 # custom error views
 def bad_request(request):
-    userOfficeData = json.loads(request.session["userOfficeData"])
-    groupName = userOfficeData["designation"]
-    username = request.session["userName"]
-    return render(request, 'error/400.html', {"group": groupName,"user":username})
+    if 'userName' in request.session:
+        userOfficeData = json.loads(request.session["userOfficeData"])
+        groupName = userOfficeData["designation"]
+        username = request.session["userName"]
+        return render(request, 'error/400.html', {"group": groupName,"user":username})
+    else:
+        return render(request, 'error/400error.html')
+
 
 def permission_denied(request):
-    userOfficeData = json.loads(request.session["userOfficeData"])
-    groupName = userOfficeData["designation"]
-    username = request.session["userName"]
-    return render(request, 'error/403.html',{"group": groupName,"user":username})
+    if 'userName' in request.session:
+        userOfficeData = json.loads(request.session["userOfficeData"])
+        groupName = userOfficeData["designation"]
+        username = request.session["userName"]
+        return render(request, 'error/403.html',{"group": groupName,"user":username})
+    else:
+        return render(request, 'error/403error.html')
 
 def page_not_found(request):
-    userOfficeData = json.loads(request.session["userOfficeData"])
-    groupName = userOfficeData["designation"]
-    username = request.session["userName"]
-    return render(request, 'error/404.html',{"group": groupName,"user":username})
+    if 'userName' in request.session:
+        userOfficeData = json.loads(request.session["userOfficeData"])
+        groupName = userOfficeData["designation"]
+        username = request.session["userName"]
+        return render(request, 'error/404.html', {"group": groupName, "user": username})
+    else:
+        return render(request, 'error/404error.html')
 
 def server_error(request):
-    userOfficeData = json.loads(request.session["userOfficeData"])
-    groupName = userOfficeData["designation"]
-    username = request.session["userName"]
-    return render(request, 'error/500.html', {"group":groupName,"user":username})
+    if 'userName' in request.session:
+        userOfficeData = json.loads(request.session["userOfficeData"])
+        groupName = userOfficeData["designation"]
+        username = request.session["userName"]
+        return render(request, 'error/500.html', {"group":groupName,"user":username})
+    else:
+        return render(request, 'error/500error.html')
 
 def connection_timeout(request):
-    userOfficeData = json.loads(request.session["userOfficeData"])
-    groupName = userOfficeData["designation"]
-    username = request.session["userName"]
-    return render(request, 'error/522.html', {"group":groupName,"user":username})
+    if 'userName' in request.session:
+        userOfficeData = json.loads(request.session["userOfficeData"])
+        groupName = userOfficeData["designation"]
+        username = request.session["userName"]
+        return render(request, 'error/522.html', {"group":groupName,"user":username})
+    else:
+        return render(request, 'error/522error.html')
 
 
 def service_unavailable(request):
-    userOfficeData = json.loads(request.session["userOfficeData"])
-    groupName = userOfficeData["designation"]
-    username = request.session["userName"]
-    return render(request, 'error/503.html', {"group":groupName,"user":username})
+    if 'userName' in request.session:
+        userOfficeData = json.loads(request.session["userOfficeData"])
+        groupName = userOfficeData["designation"]
+        username = request.session["userName"]
+        return render(request, 'error/503.html', {"group":groupName,"user":username})
+    else:
+        return render(request, 'error/503error.html')
